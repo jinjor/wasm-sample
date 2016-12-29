171308a77305b8e901831b0ff60d409b112dc572
jinjor <jinjorweb@gmail.com>
2016-12-30 02:31:12 +0900

    simplify .cc version

--- sample.cc
+++ sample.cc
@@ -1,12 +1,4 @@
-#ifdef _WIN32
-#  define EXPORT __declspec(dllexport)
-#  define IMPORT __declspec(dllimport)
-#else
-#  define EXPORT __attribute__ ((visibility ("default")))
-#  define IMPORT __attribute__ ((visibility ("default")))
-#endif
-
 extern "C" {
-  IMPORT void puts(char*);
-  EXPORT void hello() { puts("Hello World!"); }
+  void puts(char*);
+  void hello() { puts("Hello World!"); }
 }
